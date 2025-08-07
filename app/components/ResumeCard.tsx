151ddaaa62ac8353b928908380a7cb1c14f570a2
Sejal Loya <sejalloya9@gmail.com>
2025-08-07 14:14:15 +0530

    feat: added homepage resume cards and defined types

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const ResumeCard = ({resume}:{resume:Resume}) => {
+  return (
+    <div>
+      <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000'>
+        <div className='flex flex-col gap-2'>
+            <h2 className='text-black font-bold break-words'>
+                {resume.companyName}
+            </h2>
+            <h3 className='text-lg break-words text-gray-500'>{resume.jobTitle}</h3>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default ResumeCard
